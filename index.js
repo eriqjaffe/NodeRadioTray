6edeef4a0ab1cfb4a05bba2b3a97c5db15abfc9f
eriqjaffe <eriqjaffe@gmail.com>
2022-01-18 15:14:34 -0600

    removed audic, got StreamCreateURL working

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
 const { app, Menu, Tray } = require('electron')
-const Audic = require("audic-forked")
 var bass = require("bassaudio-updated");
 var basslib = new bass();
 
 basslib.EnableTags(true);
-var tagsEnabled = basslib.TagsEnabled();
 
 let tray = null
-
-var player = null;
-
-//var audic = new Audic("");
-
-var init = basslib.BASS_Init(
-  -1,
-  44100,
-  basslib.BASS_Initflags.BASS_DEVICE_STEREO
-);
-if (init === false) {
-  console.log("error at BASS_Init: " + basslib.BASS_ErrorGetCode());
-  process.exit();
-} else {
-  console.log("Bass initialized");
-}
 
 var icon = process.platform === "win32" ? './images/icons8_radio_tower_34495e.ico' : './images/icons8_radio_tower_34495e.png'
 
@@ -32,43 +14,13 @@ const createTray = () => {
     { 
         label: 'Bagel Radio', 
         click: async() => {
-            //audic.destroy();
-            //audic = new Audic("https://ais-sa3.cdnstream1.com/2606_128.aac")
-            //audic = new Audic("E:\\rock\\A\\A Giant Dog\\Toy\\03. Bendover.mp3")
-            //await audic.play();
-            //tray.setToolTip(audic.src)
-            var chan = basslib.BASS_StreamCreateFile(0, "E:\\rock\\A\\A Giant Dog\\Toy\\03. Bendover.mp3", 0, 0, 0);
-            if (basslib.BASS_ErrorGetCode() != basslib.BASS_ErrorCode.BASS_OK) {
-              console.log("error opening file:" + basslib.BASS_ErrorGetCode());
-            }
-            try {
-              var success = basslib.BASS_ChannelPlay(chan, 0);
-              if (!success) {
-                console.log("error playing file:" + basslib.BASS_ErrorGetCode());
-              } else {
-                var artist = basslib.TAGS_Read(
-                  chan,
-                  basslib.BASS_TAGS_FORMAT_CONDITION.IF_X_THEN_A_IF_NOT_THEN_B(
-                    basslib.BASS_TAGS_FORMAT_STRINGS.SONG_ARTIST,
-                    basslib.BASS_TAGS_FORMAT_STRINGS.SONG_ARTIST,
-                    "No artist"
-                  )
-                );
-                tray.setToolTip(artist)
-              }
-            } catch (error) {
-              console.log(error)
-            }
-           
+            playStream("Bagel Radio", "https://ais-sa3.cdnstream1.com/2606_128.mp3");           
         }
     },
     { 
         label: 'Indie Pop Rocks', 
         click: async() => {
-            audic.destroy();
-            audic = new Audic("https://somafm.com/indiepop130.pls")
-            await audic.play();
-            tray.setToolTip(audic.src)
+            playStream("Indie Pop Rocks", "https://ice2.somafm.com/indiepop-128-mp3")
         }
     },
   ])
@@ -78,8 +30,43 @@ const createTray = () => {
 
 app.whenReady().then(() => {
   createTray()
-
-  
 })
 
 app.on('activate', () => {})
+
+function playStream(streamName, url) {
+  basslib.BASS_Free();
+  var init = basslib.BASS_Init(
+    -1,
+    44100,
+    basslib.BASS_Initflags.BASS_DEVICE_STEREO
+  );
+  if (init === false) {
+    console.log("error at BASS_Init: " + basslib.BASS_ErrorGetCode());
+    process.exit();
+  } else {
+    console.log("Bass initialized");
+  }
+  var stream = basslib.BASS_StreamCreateURL(url, 0, 0, null, null);
+  if (basslib.BASS_ErrorGetCode() != basslib.BASS_ErrorCode.BASS_OK) {
+    console.log("error opening file:" + basslib.BASS_ErrorGetCode());
+  }
+  try {
+    var success = basslib.BASS_ChannelPlay(stream, 0);
+    if (!success) {
+      console.log("error playing file:" + basslib.BASS_ErrorGetCode());
+    } else {
+      var artist = basslib.TAGS_Read(
+        stream,
+        basslib.BASS_TAGS_FORMAT_CONDITION.IF_X_THEN_A_IF_NOT_THEN_B(
+          basslib.BASS_TAGS_FORMAT_STRINGS.SONG_ARTIST,
+          basslib.BASS_TAGS_FORMAT_STRINGS.SONG_ARTIST,
+          "No artist"
+        )
+      );
+      tray.setToolTip(streamName)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
