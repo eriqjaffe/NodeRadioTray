4ac7156847be68aae165a6209a689216c37796c5
eriqjaffe <eriqjaffe@gmail.com>
2022-01-31 15:02:06 -0600

    moved bookmarks & images to new folder, added watch to bookmarks file

--- index.js
+++ index.js
@@ -1,116 +1,46 @@
-const { app, Menu, Tray, nativeImage, ipcRenderer } = require('electron')
+const { app, Menu, Tray, nativeImage, shell } = require('electron')
 const fs = require('fs');
 const Store = require("electron-store");
 const bass = require("bassaudio-updated");
-var basslib = new bass();
+const chokidar = require("chokidar");
+const basslib = new bass();
+const firstSoundCard = (process.platform == "win32") ? 2 : 1;
+const userData = app.getPath('userData');
+const store = new Store()
 var stream = null;
 var outputDevice = -1;
+var contextMenu = null;
+var idleIcon = null;
+var playingIcon = null;
 var _tagInfo = null;
-var defaultOuputChecked = false;
+let tray = null;
 
-const store = new Store()
+const watcher = chokidar.watch([], { awaitWriteFinish: true })
+  .on('change', function(path) {
+    reloadBookmarks();
+})
+
+initializeWatcher();
 
 basslib.EnableTags(true);
 var tagsEnabled = basslib.TagsEnabled();
 if (tagsEnabled) {
-  console.log("Tags enabled");
+  console.log("BASS: Tags enabled");
 } else {
-  console.log("Tags disabled");
+  console.log("BASS: Tags disabled");
   //process.exit();
 }
 
 pluginsLoadResults = basslib.LoadAllPlugins();
 if (pluginsLoadResults === false) {
-  console.log("Error loading plugins: " + basslib.BASS_ErrorGetCode());
+  console.log("BASS: Error loading plugins: " + basslib.BASS_ErrorGetCode());
   //process.exit();
 } else {
-  //console.log(pluginsLoadResults);
+  console.log("BASS: Plugins loaded");
 }
 
-let tray = null
-
-var genreList = loadBookmarks();
-
-var idleIcon = null;
-var playingIcon = null;
 var darkIcon = (store.get("darkicon") == true) ? true : false;
 setIconTheme(darkIcon);
-/* switch (process.platform) {
-  case "win32":
-    if (darkIcon = true) {
-      idleIcon = './images/idle.ico'
-      playingIcon = './images/playing.ico'
-    } else {
-      idleIcon = './images/idle_white.ico'
-      playingIcon = './images/playing_white.ico'
-    }
-    break;
-  case "darwin":
-    idleIcon = './images/idleTemplate.png'
-    playingIcon = './images/playingTemplate.png'
-    break;
-  case "linux":
-    if (darkIcon = true) {
-      idleIcon = './images/idle.png'
-      playingIcon = './images/playing.png'
-    } else {
-      idleIcon = './images/idle_white.png'
-      playingIcon = './images/playing_white.png'
-    }
-    break;
-  default:
-    if (darkIcon = true) {
-      idleIcon = './images/idle.png'
-      playingIcon = './images/playing.png'
-    } else {
-      idleIcon = './images/idle_white.png'
-      playingIcon = './images/playing_white.png'
-    }
-    break;
-} */
-
-var cards = basslib.getDevices();
-var cardsMenu = [];
-
-for (var i = 1; i < cards.length; i++) {
-  const cardsArr = [];
-  cardsArr.id = i;
-  cardsArr.name = cards[i].name;
-  cardsArr.typeDigital = cards[i].typeDigital,
-  cardsArr.typeDisplayPort = cards[i].typeDisplayPort,
-  cardsArr.typeHandset = cards[i].typeHandset,
-  cardsArr.typeHdmi = cards[i].typeHdmi,
-  cardsArr.typeHeadPhones = cards[i].typeHeadPhones,
-  cardsArr.typeHeadSet = cards[i].typeHeadSet,
-  cardsArr.typeLine = cards[i].typeLine,
-  cardsArr.typeMask = cards[i].typeMask,
-  cardsArr.typeMicrophone = cards[i].typeMicrophone,
-  cardsArr.typeNetwork = cards[i].typeNetwork,
-  cardsArr.typeSPDIF = cards[i].typeSPDIF,
-  cardsArr.typeSpeakers = cards[i].typeSpeakers
-  var card = {
-    label: cards[i].name + " " ,
-    type: 'radio',
-    checked: cards[i].IsDefault ? true : false,
-    click: async => { 
-      outputDevice = parseInt(cardsArr.id);
-      basslib.BASS_Free();
-      var init = basslib.BASS_Init(
-        outputDevice,
-        44100,
-        basslib.BASS_Initflags.BASS_DEVICE_STEREO
-      );
-      if (init === false) {
-        console.log("error at BASS_Init: " + basslib.BASS_ErrorGetCode());
-        process.exit();
-      } else {
-        console.log("Bass initialized");
-      }
-      playStream(store.get("lastStation"), store.get("lastURL"))
-    }
-  }
-  cardsMenu.push(card)
-}
 
 const prefsTemplate = [
   {
@@ -147,36 +77,37 @@ const prefsTemplate = [
     },
     type: "checkbox",
     checked: (store.get("logging") == true) ? true : false
-  },
-  {
+  }, */
+  /* {
     label: 'Use multimedia keys',
     click: e => {
       store.set("mmkeys", e.checked)
     },
     type: "checkbox",
     checked: (store.get("mmkeys") == true) ? true : false
-  },
-  { 
+  }, */
+  /* { 
     label: 'Back/forward keys switch stations',
     click: e => {
       store.set("stationswitcher", e.checked)
     },
     type: "checkbox",
     checked: (store.get("stationswitcher") == true) ? true : false
-  },
-  {
+  }, */
+  /* {
     label: 'Autostart with operating system'
-  }
-  {
+  }, */
+  /* {
     label: 'Enable sleep timer',
     click: openAboutWindow()
   } */
 ]
 
-const menuTemplate = [
+var menuTemplate = [
   { 
+    id: 'stationMenu',
     label: 'Stations',
-    submenu: genreList,
+    submenu: loadBookmarks(),
     icon: './images/icons8-radio-2.png'
   },
   { 
@@ -187,9 +118,23 @@ const menuTemplate = [
     icon: './images/icons8-settings.png'
   },
   { 
-    label: 'Sound Cards',
-    submenu: cardsMenu,
+    label: 'Audio Output',
+    submenu: loadCards(),
     icon: './images/icons8-audio.png'
+  },
+  { 
+    label: 'Edit Stations',
+    click: e => {
+      shell.openPath(userData+'/bookmarks.json');
+    },
+    icon: './images/icons8-maintenance.png'
+  },
+  { 
+    label: 'Reload Stations',
+    click: e => {
+      reloadBookmarks();
+    },
+    icon: './images/icons8-synchronize.png'
   },
   { 
     type: 'separator'
@@ -200,7 +145,8 @@ const menuTemplate = [
     click: async() => {
       playStream(store.get('lastStation'), store.get('lastURL'));
     },
-    icon: './images/icons8-Play.png'
+    icon: './images/icons8-Play.png',
+    visible: true
   },
   {
     label: "Stop",
@@ -210,28 +156,26 @@ const menuTemplate = [
       toggleButtons(false);
     },
     icon: './images/icons8-Stop.png',
-    enabled: false
+    visible: false
   },
   {
     label: "Next Station",
     id: "nextButton",
     icon: './images/icons8-Fast Forward.png',
-    enabled: false
+    visible: false
   },
   {
     label: "Previous Station",
     id: "previousButton",
     icon: './images/icons8-Rewind.png',
-    enabled: false
+    visible: false
   },
   { 
     type: 'separator'
   },
   {
     label: "Exit",
-    click: async() => {
-        process.exit();
-    },
+    role: "quit",
     icon: './images/icons8-cancel.png'
   }
 ]
@@ -255,48 +199,105 @@ app.on('activate', () => {})
 
 function loadBookmarks() {
   var stationMenu = [];
-  let bookmarks = JSON.parse(fs.readFileSync('bookmarks.json'));
-  for(var i = 0; i < bookmarks.length; i++) {
-    var obj = bookmarks[i];
-    var stations = []
-    for (var j = 0; j < obj.bookmark.length; j++) {
-      const tmp = []
-      tmp.name = obj.bookmark[j].name
-      tmp.url = obj.bookmark[j].url
-      tmp.img = obj.bookmark[j].img
+  try {
+    let bookmarks = JSON.parse(fs.readFileSync(userData+'/bookmarks.json'));
+    for(var i = 0; i < bookmarks.length; i++) {
+      var obj = bookmarks[i];
+      var stations = []
+      for (var j = 0; j < obj.bookmark.length; j++) {
+        const tmp = []
+        tmp.name = obj.bookmark[j].name
+        tmp.url = obj.bookmark[j].url
+        tmp.img = obj.bookmark[j].img
+        try {
+          if (tmp.img.length > 0 && fs.existsSync(userData+'/images/'+tmp.img)) {
+            var stationIcon = nativeImage.createFromPath(userData+'/images/'+tmp.img).resize({width:16})
+          } else {
+            var stationIcon = './images/icons8-radio-2.png'
+          }
+        } catch (error) {
+          console.error(error)
+        }
+        var station = {
+          label: tmp.name,
+          click: async => { playStream(tmp.name, tmp.url)},
+          icon: stationIcon
+        }
+        stations.push(station)
+      }
       try {
-        if (tmp.img.length > 0 && fs.existsSync('./images/'+tmp.img)) {
-          var stationIcon = nativeImage.createFromPath(__dirname+'/images/'+tmp.img).resize({width:16})
+        if (obj.img.length > 0 && fs.existsSync(userData+'/images/'+obj.img)) {
+          var genreIcon = nativeImage.createFromPath(userData+'/images/'+obj.img).resize({width:16})
         } else {
-          var stationIcon = './images/icons8-radio-2.png'
+          var genreIcon = './images/icons8-radio-2.png'
         }
       } catch (error) {
-        console.error(error)
-      }
-      var station = {
-        label: tmp.name,
-        click: async => { playStream(tmp.name, tmp.url)},
-        icon: stationIcon
-      }
-      stations.push(station)
-    }
-    try {
-      if (obj.img.length > 0 && fs.existsSync('./images/'+obj.img)) {
-        var genreIcon = nativeImage.createFromPath(__dirname+'/images/'+obj.img).resize({width:16})
-      } else {
         var genreIcon = './images/icons8-radio-2.png'
       }
-    } catch (error) {
-      var genreIcon = './images/icons8-radio-2.png'
+      var genre = {
+        label: obj.name,
+        submenu: stations,
+        icon: genreIcon
+      }
+      stationMenu.push(genre)
     }
-    var genre = {
-      label: obj.name,
-      submenu: stations,
-      icon: genreIcon
-    }
-    stationMenu.push(genre)
+    console.log("Bookmarks loaded")
+    return stationMenu;
+  } catch (error) {
+    console.log(error)
   }
-  return stationMenu;
+}
+
+function reloadBookmarks() {
+  menuTemplate[0].submenu = loadBookmarks();
+  contextMenu = Menu.buildFromTemplate(menuTemplate)
+  tray.setContextMenu(contextMenu)
+}
+
+function loadCards() {
+  var cards = basslib.getDevices();
+  var cardsMenu = [];
+
+  for (var i = firstSoundCard; i < cards.length; i++) {
+    const cardsArr = [];
+    cardsArr.id = i;
+  /*   cardsArr.name = cards[i].name;
+    cardsArr.typeDigital = cards[i].typeDigital,
+    cardsArr.typeDisplayPort = cards[i].typeDisplayPort,
+    cardsArr.typeHandset = cards[i].typeHandset,
+    cardsArr.typeHdmi = cards[i].typeHdmi,
+    cardsArr.typeHeadPhones = cards[i].typeHeadPhones,
+    cardsArr.typeHeadSet = cards[i].typeHeadSet,
+    cardsArr.typeLine = cards[i].typeLine,
+    cardsArr.typeMask = cards[i].typeMask,
+    cardsArr.typeMicrophone = cards[i].typeMicrophone,
+    cardsArr.typeNetwork = cards[i].typeNetwork,
+    cardsArr.typeSPDIF = cards[i].typeSPDIF,
+    cardsArr.typeSpeakers = cards[i].typeSpeakers */
+    var card = {
+      label: cards[i].name + " " ,
+      type: 'radio',
+      checked: cards[i].IsDefault ? true : false,
+      click: async => { 
+        outputDevice = parseInt(cardsArr.id);
+        basslib.BASS_Free();
+        var init = basslib.BASS_Init(
+          outputDevice,
+          44100,
+          basslib.BASS_Initflags.BASS_DEVICE_STEREO
+        );
+        if (init === false) {
+          console.log("error at BASS_Init: " + basslib.BASS_ErrorGetCode());
+          process.exit();
+        } else {
+          console.log("BASS: Bass initialized on device " + outputDevice);
+        }
+        playStream(store.get("lastStation"), store.get("lastURL"))
+      }
+    }
+    cardsMenu.push(card)
+  }
+  return cardsMenu;
 }
 
 function playStream(streamName, url) {
@@ -312,16 +313,16 @@ function playStream(streamName, url) {
     console.log("error at BASS_Init: " + basslib.BASS_ErrorGetCode());
     process.exit();
   } else {
-    console.log("Bass initialized");
+    console.log("BASS: Bass initialized on device " + outputDevice);
   }
   stream = basslib.BASS_StreamCreateURL(url, 0, 0, null, null);
   if (basslib.BASS_ErrorGetCode() != basslib.BASS_ErrorCode.BASS_OK) {
-    console.log("error opening file:" + basslib.BASS_ErrorGetCode());
+    console.log("BASS: Error opening file:" + basslib.BASS_ErrorGetCode());
   }
   try {
     var success = basslib.BASS_ChannelPlay(stream, 0);
     if (!success) {
-      console.log("error playing file:" + basslib.BASS_ErrorGetCode());
+      console.log("BASS: Error playing file:" + basslib.BASS_ErrorGetCode());
     } else {
       toggleButtons(true);
       store.set('lastStation',streamName);
@@ -339,7 +340,7 @@ function playStream(streamName, url) {
   } catch (error) {
     console.log(error)
   }
-}5
+}
 
 function toggleButtons(state) {
   playButton = contextMenu.getMenuItemById('playButton')
@@ -347,24 +348,16 @@ function toggleButtons(state) {
   nextButton = contextMenu.getMenuItemById('nextButton')
   previousButton = contextMenu.getMenuItemById('previousButton')
   if (state == true) {
-    playButton.enabled = false;
-    //playButton.setImage('./images/icons8-Play_disabled.png');
-    stopButton.enabled = true;
-    //stopButton.setImage('./images/icons8-Stop.png');
-    nextButton.enabled = true;
-    //nextButton.setImage('./images/icons8-Fast Foward.png');
-    previousButton.enabled = true;
-    //nextButton.setImage('./images/icons8-Rewind.png');
+    playButton.visible = false;
+    stopButton.visible = true;
+    nextButton.visible = true;
+    previousButton.visible = true;
     tray.setImage(playingIcon);
   } else {
-    playButton.enabled = true;
-    //playButton.setImage('./images/icons8-Play.png');
-    stopButton.enabled = false;
-    //stopButton.setImage('./images/icons8-Stop_disabled.png');
-    nextButton.enabled = false;
-    //nextButton.setImage('./images/icons8-Fast Foward_disabled.png');
-    previousButton.enabled = false;
-    //nextButton.setImage('./images/icons8-Rewind_disabled.png');
+    playButton.visible = true;
+    stopButton.visible = false;
+    nextButton.visible = false;
+    previousButton.visible = false;
     tray.setImage(idleIcon);
   }
 }
@@ -403,4 +396,28 @@ function setIconTheme(checked) {
       }
       break;
   }
+}
+
+function initializeWatcher() {
+  if (!fs.existsSync(userData+'/bookmarks.json')) {
+    try {
+      console.log("User bookmarks file not found, creating...")
+      fs.copyFileSync(__dirname+"/bookmarks.json", userData+'/bookmarks.json');
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  if (!fs.existsSync(userData+'/images')) {
+    try {
+      console.log("User images directory not found, creating...")
+      fs.mkdirSync(userData+'/images');
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  watcher.add(userData+'/bookmarks.json',
+    { awaitWriteFinish: true })
+    .on('ready', function() {
+      console.log('Watching bookmark file:', watcher.getWatched());
+  });
 }
