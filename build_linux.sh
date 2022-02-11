e4f306adaca67fd7f8a174427c12db762f5c896b
eriqjaffe <eriqjaffe@gmail.com>
2022-02-11 11:58:37 -0600

    added linux build stuff

---
+++ build_linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yarn run pack:linux64
+yarn run pack:arm
+electron-installer-debian --src=./dist/NodeRadioTray-linux-x64/ --dest dist/installers --arch amd64
+electron-installer-debian --src=./dist/NodeRadioTray-linux-armv7l/ --dest dist/installers --arch armv7l
+
