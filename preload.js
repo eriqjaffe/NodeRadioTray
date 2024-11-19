3ef698db90cb2e94e1c88f905afb544fa46f58b5
eriqjaffe <eriqjaffe@gmail.com>
2024-11-19 09:32:36 -0600

    remove unnecessary file

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  setTooltipWidth: (width) => ipcRenderer.send('set-tooltip-width', width)
-});
